fb5a4eb975bb491e4e3f9d41db78a3e65992e6ce
Erik Jarem <erik.jarem@kongsbergdigital.com>
2023-12-05 16:03:07 +0100

    Day 4 part 1

--- 2023/four/main.cpp
+++ 2023/four/main.cpp
@@ -1,6 +1,10 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <sstream>
+#include "../../../../../../usr/include/c++/11/bits/stream_iterator.h"
+
 
 using namespace std;
 
@@ -8,7 +12,6 @@ uint32_t first(ifstream &stream);
 
 typedef struct {
     uint32_t cardName;
-    vector<uint32_t> numbers;
 } Card;
 
 int main(int argc, char *argv[])
@@ -22,23 +25,153 @@ int main(int argc, char *argv[])
 
     first(fileinput);
 
+    // string s = "What is the right way to split a string into a vector of strings";
+    
+
     return 0;
 }
 
 uint32_t first(ifstream &stream)
 {
     string line;
-
+    uint32_t sum = 0;
+    int gameNumber = 1;
     while(getline(stream, line))
     {
-        cout << line << endl;
 
-        int delimPos = line.find(':');
-        if (delimPos)
+        int colonSplit = line.find(':');
+        if (colonSplit)
         {
-            cout << line.substr(0, delimPos) << endl;
+            string allNumbers = line.substr(colonSplit, line.size() - colonSplit);
+            
+            int barSplit = allNumbers.find('|');
+
+            if (barSplit)
+            {
+                vector<uint32_t> winningNumberVector;
+                vector<uint32_t> submittedNumberVector;
+                stack<uint32_t> temp;
+                string winningNumbers = allNumbers.substr(0, barSplit);
+
+                // GATHER WINNING NUMBERS
+                for (int i = 0; i < winningNumbers.size(); i++)
+                {
+                    if (isdigit(winningNumbers[i]))
+                    {
+                        temp.push((int)winningNumbers[i] - 48);
+                    }
+                    else if (!temp.empty())
+                    {
+                        uint32_t lol = 0;
+                        uint32_t multiplier = 1;
+                        while (!temp.empty())
+                        {
+                            lol +=  temp.top() * multiplier;
+                            multiplier *= 10;
+                            temp.pop();
+                        }
+                        winningNumberVector.push_back(lol);
+                    }
+                }
+                if (!temp.empty())
+                {
+                    uint32_t lol = 0;
+                    uint32_t multiplier = 1;
+                    while (!temp.empty())
+                    {
+                        lol +=  temp.top() * multiplier;
+                        multiplier *= 10;
+                        temp.pop();
+                    }
+                    winningNumberVector.push_back(lol);
+                }
+
+                // PRINT WINNING NUMBERS
+                // cout << "GAME " << gameNumber << ": ";
+                // for (int i = 0; i < winningNumberVector.size(); i++)
+                // {
+                //     cout << winningNumberVector[i] << ", ";
+                // }
+                // cout << endl;
+
+                string submittedNumbers = allNumbers.substr(barSplit, allNumbers.size() - barSplit);
+
+                for (int i = 0; i < submittedNumbers.size(); i++)
+                {
+                    if (isdigit(submittedNumbers[i]))
+                    {
+                        temp.push((int)submittedNumbers[i] - 48);
+                    }
+                    else if (!temp.empty())
+                    {
+                        uint32_t lol = 0;
+                        uint32_t multiplier = 1;
+                        while (!temp.empty())
+                        {
+                            lol +=  temp.top() * multiplier;
+                            multiplier *= 10;
+                            temp.pop();
+                        }
+                        submittedNumberVector.push_back(lol);
+                    }
+                }
+                if (!temp.empty())
+                {
+                    uint32_t lol = 0;
+                    uint32_t multiplier = 1;
+                    while (!temp.empty())
+                    {
+                        lol +=  temp.top() * multiplier;
+                        multiplier *= 10;
+                        temp.pop();
+                    }
+                    submittedNumberVector.push_back(lol);
+                }
+
+                if (gameNumber == 3)
+                {
+                    for (int i = 0; i < winningNumberVector.size(); i++)
+                    {
+                        cout << winningNumberVector[i] << " ";
+                    }
+                    cout << " | ";
+
+                    for (int i = 0; i < submittedNumberVector.size(); i++)
+                    {
+                        cout << submittedNumberVector[i] << " ";
+                    }
+                    cout << endl;
+                }
+
+                uint32_t count = 0;
+
+                for (int i = 0; i < submittedNumberVector.size(); i++)
+                {
+                    for (int j = 0; j < winningNumberVector.size(); j++)
+                    {
+                        if (submittedNumberVector[i] == winningNumberVector[j])
+                        {
+                            if (count == 0)
+                            {
+                                count = 1;
+                            }
+                            else 
+                            {   
+                                count *= 2;
+                            }
+                        }
+                    }
+                }
+
+                cout << "COUNT: " << count << endl;
+                sum += count;
+            }
+
         }
+
+        gameNumber++;
     }
 
+    cout << "SUM: " << sum << endl;
     return 0;
 }
