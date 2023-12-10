aa803196692e362dc96c7336d9b9182c60622729
Erik Jarem <erik.jarem@gmail.com>
2023-12-10 16:32:10 +0100

    day 6

--- 2023/six/main.cpp
+++ 2023/six/main.cpp
@@ -5,15 +5,17 @@
 
 using namespace std;
 
-int first(ifstream &stream);
-
 typedef struct {
-    int duration;   // FROM INPUT
-    int record;     // FROM INPUT
+    long duration;   // FROM INPUT
+    long record;     // FROM INPUT
     int lowest;     // LOWEST TIME TO HOLD THE BUTTON
     int highest;    // HIGHEST TIME TO HOLD THE BUTTON
     int count;      // COUNT OF WINNING ALTERNATIVES (highest - lowest, I guess..)
 } Race;
+
+int first(ifstream &stream);
+int second(ifstream &stream);
+int recoredBeater(Race race);
 
 int main(int argc, char *argv[])
 {
@@ -32,7 +34,7 @@ int main(int argc, char *argv[])
 
     string line;
 
-    first(fileinput);
+    second(fileinput);
 
     return 0;
 }
@@ -43,7 +45,7 @@ int first(ifstream &stream)
     vector<Race> races;
     vector<string> s1;
     string line;
-    int sum;
+    int sum = 1;
     vector<int> durations;
     vector<int> records;
     regex nums(R"(\d+)");
@@ -87,7 +89,96 @@ int first(ifstream &stream)
         Race a;
         a.duration = durations[i];
         a.record = records[i];
+        races.push_back(a);
+
+        a.count = recoredBeater(a);
+        cout << "COUNT: " << a.count << endl;
+
+        sum *= a.count;
     }
 
+    cout << endl << "SUM: " << sum << endl;
+
     return 0;
+}
+
+int second(ifstream &stream)
+{
+    Race race;
+    vector<string> s1;
+    string line;
+    int sum = 1;
+    string durations;
+    string records;
+    regex nums(R"(\d+)");
+
+    while(getline(stream, line))
+    {
+        s1.push_back(line);
+    }
+    
+    
+    sregex_iterator start1(s1[0].begin(), s1[0].end(), nums);
+    sregex_iterator end1;
+
+    for (sregex_iterator i = start1; i != end1; i++)
+    {
+        smatch m = *i;
+
+        for (int i = 0; i < m.size(); i++)
+        {
+            string num1 = m[i].str();
+            durations += num1;
+        }
+    }
+
+    sregex_iterator start(s1[1].begin(), s1[1].end(), nums);
+    sregex_iterator end;
+
+    for (sregex_iterator i = start; i != end; i++)
+    {
+        smatch m = *i;
+
+        for (int i = 0; i < m.size(); i++)
+        {
+            string num1 = m[i].str();
+            records += num1;
+        }
+        
+    }
+
+
+
+    race.duration = stol(durations);
+    race.record = stol(records);
+
+    cout << race.duration << " " << race.record << endl;
+
+
+    race.count = recoredBeater(race);
+    cout << "COUNT: " << race.count << endl;
+
+    sum = race.count;
+
+    cout << endl << "SUM: " << sum << endl;
+
+    return 0;
+}
+
+int recoredBeater(Race race)
+{
+    int result = 0;
+    long sum;
+    
+    for (int i = 0; i <= race.duration; i++)
+    {
+        sum = (race.duration - i) * i;
+        if (sum > race.record)
+        {
+            result++;      
+            // cout << "GAME: " << race.duration << "  DISTANCE: "  << sum << endl;
+        }
+    }
+
+    return result;
 }
